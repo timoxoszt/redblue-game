9e2192d40cf2acf959074004d0d827425e233aa0
Vi Minh Tien <20520810@gm.uit.edu.vn>
2022-08-28 14:40:54 +0700

    script view logs

---
+++ checklog.py
@@ -0,0 +1,20 @@
+import urllib.parse
+
+f = open("access1_202208280730.log", "r")
+
+while True:
+  
+  log = f.readline()
+  if log == "":
+    break
+  i = log.find("?username=")
+  if i == -1:
+    continue
+  j = log.find("HTTP/1.1")
+ 
+  params = log[i+1:j-1]
+  
+  print("Encoded: " + params)
+  url = urllib.parse.unquote(params)
+  print("Decoded: " +url)
+  print("-"*20)
